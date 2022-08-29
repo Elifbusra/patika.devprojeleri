dd5eb9f7d7781f998d9061cee45eda47b02db39f
Elifbusra <103147966+Elifbusra@users.noreply.github.com>
2022-08-29 15:37:20 +0300

    Update flattenproject.py

--- flattenproject.py
+++ flattenproject.py
@@ -1,10 +1,9 @@
-list2 = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-def flatten_list(list1):
-  for i in list1:
-    if (type(list1) == list):
-      flatten(i)
-    else:
-      list.append(i)
-  return list
-flatten(list1)
-
+listnew = []
+def flatten_list(liste):
+    for i in liste:
+        if type(i) == list:
+            flatten_list(i)
+        else:
+            listnew.append(i)
+    return listnew
+print(listnew)
